284e27cd77af3f68c12837ba7700feb56dcd1d04
Yangpaul-583 <yangpaul583@gmail.com>
2025-12-14 10:30:33 +0800

    Initial Commit

---
+++ Apex/ApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexApp.swift
+//  Apex
+//
+//  Created by Yang Paul on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
